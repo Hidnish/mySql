5106c01c20a42beb758bf98fd1dbab4b5e7615e4
Hidnish <hideonishimura@live.it>
2021-07-08 14:28:05 +0000

    some commit

--- mfp.py
+++ mfp.py
@@ -7,18 +7,19 @@ username = os.getenv("C9_USER")
 
 # connect to database
 connection = pymysql.connect(host='localhost',
-                            user=username,
-                            password='',
-                            db='Chinook')
+                             user=username,
+                             password='',
+                             db='Chinook')
 
 
 try:
     with connection.cursor() as cursor:
-        row = ("Bob", 21, "1990-06-02 23:04:56")
-        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        list_of_names = ['Fred', 'Fred']
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings), list_of_names)
         connection.commit()
         # Note that the above will still display a warning (not error) if the
         # table already exists
 finally:
-    # always close the connection 
+    # always close the connection
     connection.close()
